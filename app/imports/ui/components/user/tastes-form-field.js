91d084bf3a8f160d20c8a54ba964c2e18e447e1b
richard kahakui <rkahakui@hawaii.edu>
2017-04-19 17:13:27 -1000

    We Gucci boys!
    I noticed though that the tastes filter options on the left is
    unaligned and should be changed in another or future milestone issue.

--- app/imports/ui/components/user/tastes-form-field.js
+++ app/imports/ui/components/user/tastes-form-field.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 
-Template.Tastes_Form_Fieldd.onRendered(function onRendered() {
+Template.Tastes_Form_Field.onRendered(function onRendered() {
   this.$('.dropdown').dropdown();
 });
 
